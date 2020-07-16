79275d35fdcf9039993eb8af157e2bec9c42af49
vrettasm <vrettasm@gmail.com>
2020-07-16 21:14:19 +0200

    Initial commit.

---
+++ code/src/simulation.py
@@ -0,0 +1,106 @@
+import h5py
+from pathlib import Path
+
+import numpy as np
+
+class Simulation(object):
+    """
+    Main simulation class. The normal workflow is as follow:
+
+    1) Create a simulation object. You can also give a name
+    that will be used when saving the data.
+
+        >> sim_01 = Simulation("Sim_01")
+
+    2) Setup its simulation parameters.
+
+        >> sim_01.setup(params)
+
+    3) Run the simulation (this step might take a while).
+
+        >> sim_01.run()
+
+    4) Finally save the results in a hdf5 (compressed) file.
+
+        >> sim_01.save()
+    """
+
+    __slots__ = ("name", "m_data", "rng", "output")
+
+    def __init__(self, name=None, seed=None):
+        """
+        Default constructor of the Simulation class.
+
+        :param name: (string) is optional but it will be used for constructing
+        a meaningful filename to save the results at the end of the simulation.
+
+        :param seed: (int) is used to initialize the random generator. If None,
+        the generator will be initialized at random from the OS.
+        """
+        # Check if a simulation name has been given.
+        if name is not None:
+            self.name = name
+        else:
+            self.name = "ID_None"
+        # _end_if_
+
+        # This dictionary will hold all the simulation data.
+        self.m_data = {}
+
+        # Create a random number generator.
+        if seed is not None:
+            self.rng = np.random.default_rng(seed)
+        else:
+            self.rng = np.random.default_rng()
+        # _end_if_
+
+        # Place holder for the output storage.
+        self.output = {}
+    # _end_def_
+
+    def setup(self, params, data):
+        pass
+    # _end_def_
+
+    def run(self):
+        pass
+    # _end_def_
+
+    def save(self):
+        pass
+    # _end_def_
+
+# _end_class_
+
+# Auxiliary function.
+def load(filename=None):
+    """
+    Loads the simulation data that have been stored in the hdf5 file
+    using the object's method saveResults().
+
+    :param filename: (string) is the '.h5' file that contains the data.
+
+    :return: a dictionary with all the data.
+    """
+
+    # Check if we have given input file.
+    if filename is None:
+        raise RuntimeError(" load_data: No input file is given.")
+    # _end_if_
+
+    # Simulation data.
+    sim_data = {}
+
+    # Open the file for read only.
+    with h5py.File(Path(filename), 'r') as input_file:
+
+        # Extract all the data.
+        for key in input_file:
+            sim_data[key] = np.array(input_file[key])
+        # _end_for_
+
+    # _end_with_
+
+    # Return the dictionary.
+    return sim_data
+# _end_def_
---
+++ code/vgpa_main.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+
+import sys
+import pandas as pd
+from pathlib import Path
+from code.src.simulation import Simulation
+
+# INFO:
+__author__ = "Michail Vrettas, PhD"
+__email__ = "michail.vrettas@gmail.com"
+
+
+def validateInputParametersFile(filename):
+    """
+    Validates an input (json) file to check if it contains the required keys.
+    It does not validate the values of the keys.
+
+    :param filename: Is a "Path" object that contains the input model parameters
+    for the simulation.
+
+    :return: A dictionary loaded from the input file.
+
+    :raises ValueError: if a keyword is missing from the file.
+    """
+
+    # Import locally the json package.
+    import json
+
+    # Open the file in "Read Only" mode.
+    with open(filename, 'r') as input_file:
+
+        # Load the model parameters.
+        model_params = json.load(input_file)
+
+        # Required keys in the json file.
+        required_keys = ("x01", "x02")
+
+        # Check the keywords for membership in the file.
+        for k in required_keys:
+
+            # The order in here doesn't matter.
+            if k not in model_params:
+                raise ValueError(" Key: {0}, is not given.".format(k))
+            # _end_if_
+
+        # _end_for_
+
+        # Show message.
+        print(" Model parameters are given correctly.")
+    # _end_with_
+
+    # The dictionary will contain all the input parameters.
+    return model_params
+# _end_def_
+
+
+# Main function.
+def main(params_file=None, data_file=None):
+    """
+    As the name suggests, this is the main function that is called to initiate the
+    simulation run.
+
+    :param params_file: (string) that points to the input file for the parameters.
+
+    :param data_file: (string) that points to the input file for the water data.
+
+    :return: None.
+    """
+
+    # Check if we got model parameters.
+    if params_file is not None:
+        try:
+            # Make sure params_file is a Path object.
+            params_file = Path(params_file)
+
+            # Check if everything is ok.
+            params = validateInputParametersFile(params_file)
+        except ValueError as e0:
+            # Show the error message.
+            print(e0)
+
+            # Exit the program.
+            sys.exit(1)
+        # _end_try_
+    else:
+        print(" The simulation can't run without input parameters.")
+
+        # Exit the program.
+        sys.exit(1)
+    # _end_if_
+
+    # Check if we got simulation water data. Make sure its a Path object.
+    if data_file is not None:
+        data_file = Path(data_file)
+    else:
+        data_file = Path(params["Data_Filename"])
+    # _end_if_
+
+    # Display where we got the water data from.
+    print(" Simulation water data file: {0}".format(data_file))
+
+    try:
+        # Open the water data in "Read Only" mode.
+        with open(data_file, 'r') as input_file:
+            # The file should have four columns.
+            water_data = pd.read_csv(input_file,
+                                     names=["ID", "Datenum", "Precipitation_cm", "WTD_m"])
+        # _end_with_
+
+        # Get the output name from the file.
+        output_name = params["Output_Name"]
+
+        # If nothing has been given set a default name.
+        if output_name is None:
+            output_name = "Sim_01"
+        # _end_if_
+
+        # Create a simulation object.
+        sim_01 = Simulation(output_name)
+
+        # Setup its parameters and water data.
+        sim_01.setup(params, water_data)
+
+        # Run the simulation.
+        sim_01.run()
+
+        # Save the results.
+        sim_01.save()
+    except Exception as e1:
+        print(e1)
+
+        # Exit the program.
+        sys.exit(1)
+    # _end_try_
+
+# _end_maim_
+
+
+# Run the script.
+if __name__ == "__main__":
+
+    # Check if we have given input parameters.
+    if len(sys.argv) > 1:
+        # Local import.
+        import argparse
+
+        # Create a parser object
+        parser = argparse.ArgumentParser(description=" VGPA (1.0) ")
+
+        # Input file with simulation parameters.
+        parser.add_argument("--params",
+                            help=" Input file (.json) with simulation parameters.")
+
+        # Input file with simulation data.
+        parser.add_argument("--data",
+                            help=" Input file (.csv) with simulation data.")
+
+        # Parse the arguments.
+        args = parser.parse_args()
+
+        # Call the main function.
+        main(args.params, args.data)
+
+        # Display final info.
+        print(' Simulation completed.')
+    else:
+        sys.exit('Error: Not enough input parameters.')
+    # _end_if_
+
+# _end_program_
