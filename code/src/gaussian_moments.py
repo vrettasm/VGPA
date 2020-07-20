3d03e607ba7faae1fcc933f729bc319dbeb7d6f1
vrettasm <vrettasm@gmail.com>
2020-07-20 13:17:04 +0200

    Update Gaussian moments.

--- code/src/gaussian_moments.py
+++ code/src/gaussian_moments.py
@@ -1,23 +1,164 @@
+import numpy as np
 
 class GaussianMoments(object):
     # ...
-    __slots__ = ("m_arr", "v_arr")
+    __slots__ = ("m_arr", "v_arr", "n_size")
 
     def __init__(self, m_arr, v_arr):
+        """
+
+        :param m_arr:
+
+        :param v_arr:
+
+        """
+
+        # Store the marginal means and variances.
         self.m_arr = m_arr
         self.v_arr = v_arr
+
+        # Get the size of the arrays.
+        self.n_size = m_arr.shape[0]
     # _end_def_
 
     def __call__(self, order=0):
-        pass
+        """
+        Compute the correct un-centered moment up to 8-th order.
+
+        :param order: of the un-centered Gaussian moment.
+
+        :return: the un-centered Gaussian moment.
+
+        :raises ValueError: if the input order is out of bounds.
+        """
+
+        if order == 0:
+            x_out = np.ones(self.n_size)
+        elif order == 1:
+            x_out = self.m_arr
+        elif order == 2:
+            x_out = self.m_arr ** 2 + self.v_arr
+        elif order == 3:
+            x_out = self.m_arr ** 3 +\
+                    3 * self.m_arr * self.v_arr
+        elif order == 4:
+            x_out = self.m_arr ** 4 +\
+                    6 * (self.m_arr ** 2) * self.v_arr +\
+                    3 * (self.v_arr ** 2)
+        elif order == 5:
+            x_out = self.m_arr ** 5 +\
+                    10 * (self.m_arr ** 3) * self.v_arr +\
+                    15 * self.m_arr * (self.v_arr ** 2)
+        elif order == 6:
+            x_out = self.m_arr ** 6 +\
+                    15 * (self.m_arr ** 4) * self.v_arr +\
+                    45 * (self.m_arr ** 2) * (self.v_arr ** 2) +\
+                    15 * (self.v_arr ** 3)
+        elif order == 7:
+            x_out = self.m_arr ** 7 +\
+                    21 * (self.m_arr ** 5) * self.v_arr +\
+                    105 * (self.m_arr ** 3) * (self.v_arr ** 2) +\
+                    105 * self.m_arr * (self.v_arr ** 3)
+        elif order == 8:
+            x_out = self.m_arr ** 8 +\
+                    28 * (self.m_arr ** 6) * self.v_arr +\
+                    210 * (self.m_arr ** 4) * (self.v_arr ** 2) +\
+                    420 * (self.m_arr ** 2) * (self.v_arr ** 3) +\
+                    105 * (self.v_arr ** 4)
+        else:
+            raise ValueError(" {0}: Wrong order value. Use 0-8.".format(self.__class__.__name__))
+        # _end_if_
+
+        return x_out
     # _end_def_
 
-    def dM(self, order=0):
-        pass
+    def dM(self, order=1):
+        """
+        Compute the derivative with respect to the marginal means,
+        of the un-centered moment, up to 8-th order.
+
+        :param order: of the un-centered Gaussian moment.
+
+        :return: the derivative with respect to the marginal means.
+
+        :raises ValueError: if the input order is out of bounds.
+        """
+
+        if order == 1:
+            x_out = np.ones(self.n_size)
+        elif order == 2:
+            x_out = 2 * self.m_arr
+        elif order == 3:
+            x_out = 3 * (self.m_arr ** 2 + self.v_arr)
+        elif order == 4:
+            x_out = 4 * (self.m_arr ** 3 +
+                         3 * self.m_arr * self.v_arr)
+        elif order == 5:
+            x_out = 5 * (self.m_arr ** 4 +
+                         6 * (self.m_arr ** 2) * self.v_arr +
+                         3 * (self.v_arr ** 2))
+        elif order == 6:
+            x_out = 6 * (self.m_arr ** 5 +
+                         10 * (self.m_arr ** 3) * self.v_arr +
+                         15 * self.m_arr * (self.v_arr ** 2))
+        elif order == 7:
+            x_out = 7 * (self.m_arr ** 6 +
+                         15 * (self.m_arr ** 4) * self.v_arr +
+                         45 * (self.m_arr ** 2) * (self.v_arr ** 2) +
+                         15 * (self.v_arr ** 3))
+        elif order == 8:
+            x_out = 8 * (self.m_arr ** 7 +
+                         21 * (self.m_arr ** 5) * self.v_arr +
+                         105 * (self.m_arr ** 3) * (self.v_arr ** 2) +
+                         105 * self.m_arr * (self.v_arr ** 3))
+        else:
+            raise ValueError(" {0}: Wrong order value. Use 1-8.".format(self.__class__.__name__))
+        # _end_if_
+
+        return x_out
     # _end_def_
 
-    def dS(self, order=0):
-        pass
+    def dS(self, order=1):
+        """
+        Compute the derivative with respect to the marginal variances,
+        of the un-centered moment, up to 8-th order.
+
+        :param order: of the un-centered Gaussian moment.
+
+        :return: the derivative with respect to the marginal variances.
+
+        :raises ValueError: if the input order is out of bounds.
+        """
+
+        if order == 1:
+            x_out = np.zeros(self.n_size)
+        elif order == 2:
+            x_out = np.ones(self.n_size)
+        elif order == 3:
+            x_out = 3 * self.m_arr
+        elif order == 4:
+            x_out = 6 * (self.m_arr ** 2 + self.v_arr)
+        elif order == 5:
+            x_out = 10 * (self.m_arr ** 3) +\
+                    30 * (self.m_arr * self.v_arr)
+        elif order == 6:
+            x_out = 15 * (self.m_arr ** 4) +\
+                    90 * (self.m_arr ** 2) * self.v_arr +\
+                    45 * (self.v_arr ** 2)
+        elif order == 7:
+            x_out = 21 * (self.m_arr ** 5) +\
+                    210 * (self.m_arr ** 3) * self.v_arr +\
+                    315 * self.m_arr * (self.v_arr ** 2)
+        elif order == 8:
+            x_out = 28 * (self.m_arr ** 6) +\
+                    420 * (self.m_arr ** 4) * self.v_arr +\
+                    1260 * (self.m_arr ** 2) * (self.v_arr ** 2) +\
+                    420 * (self.v_arr ** 3)
+        else:
+            raise ValueError(" {0}: Wrong order value. Use 1-8.".format(self.__class__.__name__))
+        # _end_if_
+
+        return x_out
     # _end_def_
 
 # _end_class_
