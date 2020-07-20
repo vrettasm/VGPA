3c46573f466787c2a35048da43f092bedbc1a1bf
vrettasm <vrettasm@gmail.com>
2020-07-20 18:52:48 +0200

    Added test for the SCG class.

---
+++ code/tests/test_scg.py
@@ -0,0 +1,66 @@
+import unittest
+import numpy as np
+from code.numerics.optim_scg import SCG
+from code.numerics.utilities import finite_diff
+
+
+class TestSCG(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        print(" >> TestSCG - START -")
+    # _end_def_
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print(" >> TestSCG - FINISH -")
+    # _end_def_
+
+    def test_sphere_fun(self):
+        # Sphere function.
+        fun = lambda x: np.sum(x ** 2)
+
+        # Gradient (numerical).
+        gradf = lambda x: finite_diff(fun, x)
+
+        # Initial random point.
+        x0 = np.random.randn(5)
+
+        # Create the SCG.
+        optim_fun = SCG(fun, gradf)
+
+        # Run the optimization with default parameters.
+        x_opt, fx_opt = optim_fun(x0)
+
+        # The global minimum should be zero.
+        self.assertTrue(np.abs(fx_opt - 0.0) <= 1.0E-8)
+
+        # Also the position 'x' should be zero.
+        self.assertTrue(np.all(np.abs(x_opt - 0.0) <= 1.0E-8))
+    # _end_def_
+
+    def test_rosenbrock_fun(self):
+        # Sphere function.
+        fun = lambda x: 100 * (x[1] - x[0] ** 2) ** 2 + (1 - x[0]) ** 2
+
+        # Gradient (numerical).
+        gradf = lambda x: finite_diff(fun, x)
+
+        # Initial random point.
+        x0 = np.random.randn(2)
+
+        # Create the SCG, with optional parameters.
+        optim_fun = SCG(fun, gradf, {"max_it": 50, "display": True})
+
+        # Run the optimization with default parameters.
+        x_opt, fx_opt = optim_fun(x0)
+
+        # The global minimum should be zero.
+        self.assertTrue(np.abs(fx_opt - 0.0) <= 1.0e-8)
+
+        # The position 'x' should be one.
+        self.assertTrue(np.all(np.abs(x_opt - 1.0) <= 1.0e-4))
+    # _end_def_
+
+
+if __name__ == '__main__':
+    unittest.main()
