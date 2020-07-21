3745fa95fd3a2982db5fdce2e5b1577222275dbe
vrettasm <vrettasm@gmail.com>
2020-07-21 15:08:38 +0200

    Minor edits.

--- code/numerics/utilities.py
+++ code/numerics/utilities.py
@@ -25,29 +25,31 @@ def finite_diff(fun, x, *args):
     # Number of input parameters.
     dim_x = x.shape[0]
 
-    # Preallocate array.
+    # Unit vector.
+    e0 = np.zeros(dim_x)
+
+    # Gradient vector.
     grad_n = np.zeros(dim_x)
 
-    # Unit vector.
-    e = np.zeros(dim_x)
-
     # Step size.
-    h = 1.0e-6
+    h_step = 1.0e-6
 
-    # Check all directions (coordinates of x).
+    # Iterate over all dimensions of 'x'.
     for i in range(dim_x):
         # Switch ON i-th direction.
-        e[i] = 1.0
+        e0[i] = 1.0
 
-        # Move a small way in the i-th direction of x.
-        f_plus = fun(x + h * e, *args)
-        f_minus = fun(x - h * e, *args)
+        # Move a small way in the i-th direction of '+x'.
+        f_p = fun(x + h_step * e0, *args)
+
+        # Move a small way in the i-th direction of '-x'.
+        f_m = fun(x - h_step * e0, *args)
 
         # Use central difference formula.
-        grad_n[i] = 0.5 * (f_plus - f_minus) / h
+        grad_n[i] = 0.5 * (f_p - f_m) / h_step
 
-        # Switch OFF i-th direction
-        e[i] = 0.0
+        # Switch OFF i-th direction.
+        e0[i] = 0.0
     # _end_for_
 
     # Return the numerical gradient.
