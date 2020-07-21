1f7a16e5b2df020ca5b93b0c14c89fb297920750
vrettasm <vrettasm@gmail.com>
2020-07-21 12:45:41 +0200

    Added safe-log method.

--- code/numerics/utilities.py
+++ code/numerics/utilities.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.linalg import cholesky
 
+
 def finite_diff(fun, x, *args):
     """
     Calculates the approximate derivative of function "fun"
@@ -69,12 +70,19 @@ def log_det(x=None):
         raise ValueError(" log_det: Input array is Empty! ")
     # _end_if_
 
-    # Make sure input is 1-D.
-    x = np.atleast_1d(x)
+    # Make sure input is array.
+    x = np.asarray(x)
 
-    # If the input is vector.
+    # If the input is scalar.
+    if x.ndim == 0:
+        # Return from here with the log.
+        return np.log(x)
+    # _end_if_
+
+    # If the input is a 1-D vector.
     if x.ndim == 1:
-        # Transform it to diagonal matrix.
+        # Transform it to diagonal
+        # (square) matrix.
         x = np.diag(x)
     else:
         # Get the number of rows/cols.
@@ -88,4 +96,54 @@ def log_det(x=None):
 
     # More stable than: log(det(x)).
     return 2.0 * np.sum(np.log(cholesky(x).diagonal()))
+# _end_def_
+
+def safe_log(x=None):
+    """
+    This (helper) function prevents the computation of very small, or very large
+    values of logarithms that would lead to -/+ inf, by setting predefined LOWER
+    and UPPER bounds. The bounds are set as follows:
+
+        - LOWER = 1.0E-300
+        - UPPER = 1.0E+300
+
+    It is assumed that the input values lie within this range.
+
+    Example:
+        >> numpy.log(1.0E-350)
+        >> -inf
+        >>
+        >> safe_log(1.0E-350)
+        >> -690.77552789821368
+
+    :param x: input array (dim_n x dim_m).
+
+    :return: the log(x) after the values of x have been filtered (dim_n x dim_m).
+
+    :raises ValueError: if input is None.
+    """
+
+    # Prevent empty input.
+    if x is None:
+        raise ValueError(" safe_log: Input is None.")
+    # _end_if_
+
+    # Define LOWER and UPPER bounds.
+    _low_bound_ = 1.0E-300
+    _upr_bound_ = 1.0E+300
+
+    # Make sure input is an array.
+    x = np.asarray(x)
+
+    # Check scalar.
+    if x.ndim == 0:
+        x = np.maximum(np.minimum(x, _upr_bound_), _low_bound_)
+    else:
+        # Check lower / upper bounds.
+        x[x < _low_bound_] = _low_bound_
+        x[x > _upr_bound_] = _upr_bound_
+    # _end_if_
+
+    # Return the log() of the filtered input.
+    return np.log(x)
 # _end_def_
