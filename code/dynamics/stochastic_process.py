4856b69fed65ebe97a543dfd795fc491db35fbc9
vrettasm <vrettasm@gmail.com>
2020-07-22 12:25:36 +0200

    Added method that collects noisy observations.

--- code/dynamics/stochastic_process.py
+++ code/dynamics/stochastic_process.py
@@ -94,4 +94,80 @@ class StochasticProcess(object):
         return self.rand_g
     # _end_def_
 
+    def collect_obs(self, n_obs, rn, h_mask=None):
+        """
+        This function collects a number of noisy observations from a sample path
+        (trajectory). The observations are collected at equidistant time points.
+
+        :param n_obs: Observations density (# of observations per time unit).
+
+        :param rn: Observation noise (co)-variance.
+
+        :param h_mask: List that masks only the observed values.
+
+        :return: observation times / observation values (with i.i.d. white noise).
+        """
+
+        # Check if the sample path is created.
+        if self.tk is None or self.xt is None:
+            raise NotImplementedError(" {0}: Sample path or time window"
+                                      " are not created.".format(self.__class__.__name__))
+        # _end_def_
+
+        # Get the discrete time step.
+        dt = np.diff(self.tk)[0]
+
+        # Check if the required number of observations
+        # per time unit exceeds the available capacity
+        # of samples.
+        if n_obs > (1.0 / dt):
+            raise ValueError(" {0}: Observation density"
+                             " exceeds the number of samples.".format(self.__class__.__name__))
+        # _end_def_
+
+        # Total number of observations.
+        dim_m = int(np.floor(np.abs(self.tk[0] - self.tk[-1]) * n_obs))
+
+        # Number of discrete time points.
+        dim_t = self.tk.shape[0]
+
+        # Observation indexes.
+        idx = np.linspace(0, dim_t, dim_m + 2, dtype=np.int)
+
+        # Convert it to list so you can use it as index.
+        obs_t = np.array(idx[1:-1].tolist())
+
+        # Extract the complete observations (d = D) at times obs_t.
+        obs_y = np.take(self.xt, obs_t, axis=0)
+
+        # Check if a mask has been given.
+        if h_mask is not None:
+            # Here we have (d < D)
+            obs_y = obs_y[:, h_mask]
+        # _end_def_
+
+        # Dimensionality of observations.
+        dim_d = obs_y.shape[1]
+
+        # Check if (co)-variance vector/matrix is given.
+        if np.isscalar(rn):
+            # Add fixed Gaussian noise.
+            obs_y += np.sqrt(rn) * self.rand_g.standard_normal(dim_m)
+        else:
+            # Variance (diagonal) matrix.
+            if rn.ndim == 1:
+                rn = np.diag(rn)
+            # _end_if_
+
+            # Get the square root of the noise matrix.
+            sq_rn = np.sqrt(rn)
+
+            # Add fixed Gaussian noise.
+            obs_y += sq_rn.dot(self.rand_g.standard_normal(dim_d, dim_m)).T
+        # _end_def_
+
+        # Observation (times / values).
+        return obs_t, obs_y
+    # _end_def_
+
 # _end_class_
