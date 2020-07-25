81eca60b39ebf27117856a8c4d5d2c033cd9d436
vrettasm <vrettasm@gmail.com>
2020-07-25 08:13:33 +0200

    Merged and optimized.

--- code/numerics/euler.py
+++ code/numerics/euler.py
@@ -22,7 +22,7 @@ class Euler(OdeSolver):
 
     def fwd(self, *args):
         """
-        Forward solution of the ode.
+        Forward solution of the ode. This provides the interface.
 
         :param args: dictionary with the variational parameters.
 
@@ -38,67 +38,19 @@ class Euler(OdeSolver):
         s0 = p_list["s0"]
         sigma = p_list["sigma"]
 
-        # Dimensionality of the system.
-        if at.shape[-1] == 1:
-            return self.solve_fwd_1D(at, bt, m0, s0, sigma)
-        else:
-            return self.solve_fwd_nD(at, bt, m0, s0, sigma)
+        # Dimensionality flag of the system.
+        # True, if it is single dimensional.
+        single_dim = at.shape[-1] == 1
+
+        # Return the solution of the fwd-ode.
+        return self.solve_fwd(at, bt, m0, s0, sigma, single_dim)
         # _end_if_
 
     # _end_def_
 
-    def solve_fwd_1D(self, lin_a, off_b, m0, s0, sigma):
+    def solve_fwd(self, lin_a, off_b, m0, s0, sigma, single_dim=True):
         """
-        Euler integration method 1D.
-
-        :param lin_a: Linear variational parameters (dim_n x 1).
-
-        :param off_b: Offset variational parameters (dim_n x 1).
-
-        :param m0: Initial marginal mean (scalar).
-
-        :param s0: Initial marginal variance (scalar).
-
-        :param sigma: System noise variance (scalar).
-
-        :return: 1) mt: posterior means values (dim_n x 1).
-                 2) st: posterior variance values (dim_n x 1).
-        """
-
-        # Get the number of discrete time points.
-        dim_n = off_b.shape[0]
-
-        # Preallocate the return arrays.
-        mt = np.zeros(dim_n)
-        st = np.zeros(dim_n)
-
-        # Initialize the first moments.
-        mt[0], st[0] = m0, s0
-
-        # Run through all time points.
-        for k in range(dim_n - 1):
-            # Get the values at time 'tk'.
-            ak = lin_a[k]
-            bk = off_b[k]
-
-            # Marginal moments.
-            sk = st[k]
-            mk = mt[k]
-
-            # -Eq(09)- NEW "mean" point.
-            mt[k + 1] = mk + (-ak * mk + bk) * self.dt
-
-            # -Eq(10)- NEW "covariance" point.
-            st[k + 1] = sk + (-2.0 * ak * sk + sigma) * self.dt
-        # _end_for_
-
-        # Return the marginal moments.
-        return mt, st
-    # _end_def_
-
-    def solve_fwd_nD(self, lin_a, off_b, m0, s0, sigma):
-        """
-        Euler integration method nD.
+        Euler integration method. This provides the actual solution.
 
         :param lin_a: Linear variational parameters (dim_n x dim_d x dim_d).
 
@@ -110,18 +62,39 @@ class Euler(OdeSolver):
 
         :param sigma: System noise variance (dim_d x dim_d).
 
+        :param single_dim: Boolean flag. Determines which version of the
+        code will be called.
+
         :return: 1) mt: posterior means values (dim_n x dim_d).
                  2) st: posterior variance values (dim_n x dim_d x dim_d).
         """
-        # Get the dimensions.
-        dim_n, dim_d = off_b.shape
 
-        # Preallocate the return arrays.
-        mt = np.zeros((dim_n, dim_d))
-        st = np.zeros((dim_n, dim_d, dim_d))
+        # Pre-allocate memory according to single_dim.
+        if single_dim:
+            # Number of discrete time points.
+            dim_n = off_b.shape[0]
+
+            # Return arrays.
+            mt = np.zeros(dim_n)
+            st = np.zeros(dim_n)
+        else:
+            # Get the dimensions.
+            dim_n, dim_d = off_b.shape
+
+            # Return arrays.
+            mt = np.zeros((dim_n, dim_d))
+            st = np.zeros((dim_n, dim_d, dim_d))
+        # _end_if_
 
         # Initialize the first moments.
         mt[0], st[0] = m0, s0
+
+        # Discrete time step.
+        dt = self.dt
+
+        # Local copies of auxiliary functions.
+        fun_mt = self.fun_mt
+        fun_st = self.fun_st
 
         # Run through all time points.
         for k in range(dim_n - 1):
@@ -134,19 +107,19 @@ class Euler(OdeSolver):
             mk = mt[k]
 
             # -Eq(09)- NEW "mean" point.
-            mt[k + 1] = mk + self.fun_mt_nD(mk, ak, bk) * self.dt
+            mt[k + 1] = mk + fun_mt(mk, ak, bk, single_dim) * dt
 
             # -Eq(10)- NEW "covariance" point.
-            st[k + 1] = sk + self.fun_st_nD(sk, ak, sigma) * self.dt
+            st[k + 1] = sk + fun_st(sk, ak, sigma, single_dim) * dt
         # _end_for_
 
-        # Return the marginal moments.
+        # Marginal moments.
         return mt, st
     # _end_def_
 
     def bwd(self, *args):
         """
-        Backward solution of the ode.
+        Backward solution of the ode. Provides the interface.
 
         :param args: dictionary with the variational parameters.
 
@@ -162,59 +135,17 @@ class Euler(OdeSolver):
         dEobs_dm = p_list["dEobs_dm"]
         dEobs_ds = p_list["dEobs_ds"]
 
-        # Dimensionality of the system.
-        if at.shape[-1] == 1:
-            return self.solve_bwd_1D(at, dEsde_dm, dEsde_ds, dEobs_dm, dEobs_ds)
-        else:
-            return self.solve_bwd_nD(at, dEsde_dm, dEsde_ds, dEobs_dm, dEobs_ds)
-        # _end_if_
+        # Dimensionality flag of the system.
+        # True, if it is single dimensional.
+        single_dim = at.shape[-1] == 1
+
+        # Return the solution of the fwd-ode.
+        return self.solve_bwd(at, dEsde_dm, dEsde_ds, dEobs_dm, dEobs_ds, single_dim)
     # _end_def_
 
-    def solve_bwd_1D(self, lin_a, dEsde_dm, dEsde_ds, dEobs_dm, dEobs_ds):
+    def solve_bwd(self, lin_a, dEsde_dm, dEsde_ds, dEobs_dm, dEobs_ds, single_dim=True):
         """
-        Euler integration method 1D.
-
-        :param lin_a: Linear variational parameters (dim_n x 1).
-
-        :param dEsde_dm: Derivative of Esde w.r.t. m(t), (dim_n x 1).
-
-        :param dEsde_ds: Derivative of Esde w.r.t. s(t), (dim_n x 1).
-
-        :param dEobs_dm: Derivative of Eobs w.r.t. m(t), (dim_n x 1).
-
-        :param dEobs_ds: Derivative of Eobs w.r.t. s(t), (dim_n x 1).
-
-        :return: 1) lam: Lagrange multipliers for the mean  values (dim_n x 1),
-                 2) psi: Lagrange multipliers for the var values (dim_n x 1).
-        """
-
-        # Get the dimensions.
-        dim_n = dEsde_dm.shape[0]
-
-        # Preallocate the return arrays.
-        lam = np.zeros(dim_n)
-        psi = np.zeros(dim_n)
-
-        # Run through all time points.
-        for t in range(dim_n - 1, 0, -1):
-            # Get the values at time 't'.
-            at = lin_a[t]
-            lamt = lam[t]
-            psit = psi[t]
-
-            # -Eq(14)- NEW "Lambda" point.
-            lam[t - 1] = lamt - (-dEsde_dm[t] + at * lamt) * self.dt + dEobs_dm[t - 1]
-
-            # -Eq(15)- NEW "Psi" point.
-            psi[t - 1] = psit - (-dEsde_ds[t] + 2.0 * at * psit) * self.dt + dEobs_ds[t - 1]
-        # _end_for_
-
-        return lam, psi
-    # _end_def_
-
-    def solve_bwd_nD(self, lin_a, dEsde_dm, dEsde_ds, dEobs_dm, dEobs_ds):
-        """
-        Euler integration method 1D.
+        Euler integration method. Provides the actual solution.
 
         :param lin_a: Linear variational parameters (dim_n x dim_d x dim_d).
 
@@ -226,15 +157,36 @@ class Euler(OdeSolver):
 
         :param dEobs_ds: Derivative of Eobs w.r.t. s(t), (dim_n x dim_d x dim_d).
 
-        :return: 1) lam: Lagrange multipliers for the mean values (dim_n x 1),
-                 2) psi: Lagrange multipliers for the var values (dim_n x 1).
-        """
-        # Get the dimensions.
-        dim_n, dim_d = dEsde_dm.shape
+        :param single_dim: Boolean flag. Determines which version of the
+        code will be called.
 
-        # Preallocate the return arrays.
-        lam = np.zeros((dim_n, dim_d))
-        psi = np.zeros((dim_n, dim_d, dim_d))
+        :return: 1) lam: Lagrange multipliers for the mean  values (dim_n x dim_d),
+                 2) psi: Lagrange multipliers for the var values (dim_n x dim_d x dim_d).
+        """
+
+        # Pre-allocate memory according to single_dim.
+        if single_dim:
+            # Number of discrete points.
+            dim_n = dEsde_dm.shape[0]
+
+            # Return arrays.
+            lam = np.zeros(dim_n)
+            psi = np.zeros(dim_n)
+        else:
+            # Get the dimensions.
+            dim_n, dim_d = dEsde_dm.shape
+
+            # Return arrays.
+            lam = np.zeros((dim_n, dim_d))
+            psi = np.zeros((dim_n, dim_d, dim_d))
+        # _end_if_
+
+        # Discrete time step.
+        dt = self.dt
+
+        # Local copies of auxiliary functions.
+        fun_lam = self.fun_lam
+        fun_psi = self.fun_psi
 
         # Run through all time points.
         for t in range(dim_n - 1, 0, -1):
@@ -244,10 +196,10 @@ class Euler(OdeSolver):
             psit = psi[t]
 
             # -Eq(14)- NEW "Lambda" point.
-            lam[t - 1] = lamt - self.fun_lam_nD(dEsde_dm[t], at, lamt) * self.dt + dEobs_dm[t - 1]
+            lam[t - 1] = lamt - fun_lam(dEsde_dm[t], at, lamt, single_dim) * dt + dEobs_dm[t - 1]
 
             # -Eq(15)- NEW "Psi" point.
-            psi[t - 1] = psit - self.fun_psi_nD(dEsde_ds[t], at, psit) * self.dt + dEobs_ds[t - 1]
+            psi[t - 1] = psit - fun_psi(dEsde_ds[t], at, psit, single_dim) * dt + dEobs_ds[t - 1]
         # _end_for_
 
         # Lagrange multipliers.
