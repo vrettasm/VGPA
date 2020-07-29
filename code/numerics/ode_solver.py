98358bae9ee73ffd98a8328d3866ae03176f6701
vrettasm <vrettasm@gmail.com>
2020-07-29 15:03:09 +0200

    Added documentation.

--- code/numerics/ode_solver.py
+++ code/numerics/ode_solver.py
@@ -26,6 +26,17 @@ class OdeSolver(object):
     # _end_def_
 
     def fun_mt(self, mt, at, bt):
+        """
+        Auxiliary mean function.
+
+        :param mt: marginal mean m(t).
+
+        :param at: variational parameter a(t).
+
+        :param bt: variational parameter b(t).
+
+        :return: - (A * m.T) + B
+        """
         # Switch according to single_dim.
         if self.single_dim:
             return -at * mt + bt
@@ -34,6 +45,17 @@ class OdeSolver(object):
     # _end_def_
 
     def fun_st(self, st, at, sn):
+        """
+        Auxiliary co-variance function.
+
+        :param st: marginal co-variance S(t).
+
+        :param at: variational parameter a(t).
+
+        :param sn: system noise.
+
+        :return: - (A * S) - (S * A.T) + Sigma
+        """
         # Switch according to single_dim.
         if self.single_dim:
             return -2.0 * at * st + sn
@@ -42,6 +64,17 @@ class OdeSolver(object):
     # _end_def_
 
     def fun_lam(self, df_dm, at, lamt):
+        """
+        Auxiliary Lagrange function.
+
+        :param df_dm: expectation w.r.t. m(t)
+
+        :param at: variational parameter a(t).
+
+        :param lamt: Lagrange multiplier lam(t).
+
+        :return: - dEf_dm + (lam * A.T)
+        """
         # Switch according to single_dim.
         if self.single_dim:
             return -df_dm + lamt * at
@@ -50,6 +83,17 @@ class OdeSolver(object):
     # _end_def_
 
     def fun_psi(self, df_ds, at, psit):
+        """
+        Auxiliary Lagrange function.
+
+        :param df_ds: expectation w.r.t. s(t)
+
+        :param at: variational parameter a(t).
+
+        :param psit: Lagrange multiplier psi(t).
+
+        :return: - dEf_dS + (psi * A) + (A.T * psi).
+        """
         # Switch according to single_dim.
         if self.single_dim:
             return -df_ds + 2.0 * psit * at
