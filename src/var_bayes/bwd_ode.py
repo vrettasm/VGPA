2266b36a097491f567d988bfeff130b6621dbe45
michailvrettas <vrettasm@gmail.com>
2025-12-03 10:13:26 +0100

    Simplified the creation of the numerical integrator.

--- src/var_bayes/bwd_ode.py
+++ src/var_bayes/bwd_ode.py
@@ -1,8 +1,4 @@
-from src.numerics.heun import Heun
-from src.numerics.euler import Euler
-from src.numerics.runge_kutta2 import RungeKutta2
-from src.numerics.runge_kutta4 import RungeKutta4
-
+from src.numerics.utilities import num_integration
 
 class BwdOde(object):
     """
@@ -25,40 +21,24 @@ class BwdOde(object):
 
         :param single_dim: flags the ode as 1D or nD.
         """
-
         # Check if time step is positive.
-        if dt > 0.0:
-            self.dt = dt
-        else:
+        if dt <= 0.0:
             raise ValueError(f" {self.__class__.__name__}:"
                              f" Discrete time step should be strictly positive -> {dt}.")
-        # _end_if_
+        self.dt = dt
 
         # Convert method to lower-case.
         method_str = str(method).lower()
 
-        # Create the solver object.
-        if method_str == "euler":
-
-            self.solver = Euler(dt, single_dim)
-
-        elif method_str == "heun":
-
-            self.solver = Heun(dt, single_dim)
-
-        elif method_str == "rk2":
-
-            self.solver = RungeKutta2(dt, single_dim)
-
-        elif method_str == "rk4":
-
-            self.solver = RungeKutta4(dt, single_dim)
-        else:
+        try:
+            # Create the solver object.
+            self.solver = num_integration[method_str](dt, single_dim)
+        except KeyError:
             raise ValueError(f" {self.__class__.__name__}:"
                              f" Integration method is unknown -> {method}.")
-        # _end_if_
+        # _end_try_
 
-        # Get a copy of the method name (for the __str__).
+        # Copy of the method name (for the __str__).
         self.method = method
     # _end_def_
 
